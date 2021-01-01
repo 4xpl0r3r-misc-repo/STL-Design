473194f9b9b402a8ba689a588ed5bc0271b0ee0d
kanowill <kanowillz@gmail.com>
2021-01-02 02:29:53 +0800

    vector设计错误，重新开始

--- STL-Design/vector.hpp
+++ STL-Design/vector.hpp
@@ -16,12 +16,12 @@ template<typename T>
 class vector{
     class iterator;
 public:
-    vector();
-    vector(int l);
+    vector(int l=0);
     //l为初始数组长度
     vector(const vector& tar);
     ~vector();
     void operator=(const vector& tar);
+    T& operator[](int)const;
     
     T* begin()const;
     T* end()const;
@@ -33,15 +33,10 @@ public:
     
     T& back()const;
 private:
-    T *data,*tail;
+    T *data,*tail;//tail是空指针
 };
 
 /*-------------declearation above-------------*/
-
-template<typename T>
-vector<T>::vector(){
-    tail=data=NULL;
-}
 
 template<typename T>
 vector<T>::vector(int l){
@@ -57,7 +52,8 @@ vector<T>::vector(const vector& tar){
 
 template<typename T>
 vector<T>::~vector(){
-    delete[] data;
+    while(data!=tail)
+        delete data;
 }
 
 template<typename T>
@@ -73,7 +69,7 @@ T* vector<T>::begin()const{
 
 template<typename T>
 T* vector<T>::end()const{
-    return tail+1;
+    return tail;
 }
 
 template<typename T>
@@ -83,7 +79,8 @@ long vector<T>::size()const{
 
 template<typename T>
 void vector<T>::push_back(const T& tar){
-    ++tail=new T(tar);
+    tail=new T(tar);
+    tail++;
 }
 
 template<typename T>
@@ -94,6 +91,11 @@ void vector<T>::pop_back(){
 template<typename T>
 T& vector<T>::back()const{
     return *tail;
+}
+
+template<typename T>
+T& vector<T>::operator[](int steps)const{
+    return *(data+steps);
 }
 
 }
